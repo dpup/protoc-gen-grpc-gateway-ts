dd027a99e5b8367d0da6268162c1dd4023cafa12
Daniel Pupius <dan@pupi.us>
2024-05-10 17:08:13 -0700

    Consolidate all testing code into /test/ folder

--- generator/fetchtmpl.go
+++ generator/fetchtmpl.go
@@ -2,7 +2,7 @@ package generator
 
 import _ "embed"
 
-//go:embed fetch.pb.ts
+//go:embed fetchtmpl.ts
 var fetchTmplScript string
 
 var fetchTmplHeader = `{{- if not .EnableStylingCheck}}
--- scripts/gen-protos.sh
+++ scripts/gen-protos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 USE_PROTO_NAMES=${1:-"false"}
 EMIT_UNPOPULATED=${2:-"false"}
 ENABLE_STYLING_CHECK=${3:-"false"}
@@ -6,9 +7,14 @@ ENABLE_STYLING_CHECK=${3:-"false"}
 GOBIN=$(go env GOPATH)/bin
 PATH=$GOBIN:$PATH
 
-cd .. && go install && cd integration_tests && \
-	protoc -I .  -I ../.. \
-	--grpc-gateway-ts_out ./ \
+# NOTE: The parent directory is brought in because google/protobuf/empty.proto
+# has been updated to reference "protoc-gen-grpc-gateway-ts/options/ts_package.protoprotoc-gen-grpc-gateway-ts/options/ts_package.proto"
+# this seems somewhat risky.
+
+go install &&  \
+	protoc -I ./test/integration/ \
+	-I ../ \
+	--grpc-gateway-ts_out ./test/integration/ \
 	--grpc-gateway-ts_opt logtostderr=true \
 	--grpc-gateway-ts_opt loglevel=debug \
 	--grpc-gateway-ts_opt use_proto_names=$USE_PROTO_NAMES \
